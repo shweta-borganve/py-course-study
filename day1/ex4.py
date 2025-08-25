ef0efa258bea02f6941379c61ecfc809d83c2842
shweta boraganve <Shweta.borganve@gmail.com>
2025-08-24 23:57:31 -0700

    Added ex4.py code

---
+++ day1/ex4.py
@@ -0,0 +1,11 @@
+first_name = "ram"
+last_name = "sita"
+full_name = first_name + " " + last_name 
+print(full_name) 
+
+# repeting msg 
+message = "xyz " 
+print(message * 5) 
+print(message.upper())
+print(message.lower())
+print(message.strip()) 
