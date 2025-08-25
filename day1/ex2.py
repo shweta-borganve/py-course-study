0d02b9b999d8c821f4a19deb99554ce11b2acdb6
shweta boraganve <Shweta.borganve@gmail.com>
2025-08-24 23:35:47 -0700

    Added ex3.py code

--- day1/ex2.py
+++ day1/ex2.py
@@ -1,4 +1,5 @@
 def greet(name):
+    """ Return a greeting msg for the given name."""
     return "Hello, " + name
 
 message = greet("xyz")
---
+++ day1/ex3.py
@@ -0,0 +1,8 @@
+first_name = "ram"
+last_name = "sita"
+full_name = first_name + " " + last_name 
+print(full_name) 
+
+# repeting msg 
+message = "xyz" 
+print(message * 5) 
