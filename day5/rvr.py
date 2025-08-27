3ccdeeaae997072233bcc22efcfe364848653cf3
shweta boraganve <Shweta.borganve@gmail.com>
2025-08-27 16:44:58 -0700

    Added even.py

---
+++ day5/rvr.py
@@ -0,0 +1,6 @@
+def reverse_string(s):
+    return s[::-1] 
+
+word = input("Enter a string: ") 
+reversed_word = reverse_string(word) 
+print("Reversed string is:", reversed_word) 
---
+++ day5/sum.py
@@ -0,0 +1,3 @@
+num = [2,5,8] 
+total = sum(num) 
+print("Sum of the list is", total)  
