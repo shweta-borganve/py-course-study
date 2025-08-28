0b1d5004d45888e75c7ad2d3ad7f47c65b5c0d63
shweta boraganve <Shweta.borganve@gmail.com>
2025-08-27 22:13:57 -0700

    Added lrg.py

---
+++ day5/lrg.py
@@ -0,0 +1,3 @@
+num = [4,17,9,3] 
+largest = max(num) 
+print("the Maximum number is", largest) 
---
+++ day5/vowel.py
@@ -0,0 +1,3 @@
+text = "Python is fun"
+count = text.lower().count("a") + text.lower().count("e") + text.lower().count("i") + text.lower().count("o") + text.lower().count("u")
+print("Number of vowels", count) 
