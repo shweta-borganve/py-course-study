34cef42c9f9875e40f8273ccb2f617d035201537
shweta boraganve <Shweta.borganve@gmail.com>
2025-10-12 05:41:06 -0700

    Added unique_list.py this gives the present unique values in the list

---
+++ day-02/unique_list.py
@@ -0,0 +1,8 @@
+# Given list
+my_list = [2, 3, 2, 5, 3]
+
+# Convert to set to remove duplicates, then back to list
+unique_list = list(set(my_list))
+
+# Print the result
+print("Unique values:", unique_list)
