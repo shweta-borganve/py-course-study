57d0642ae5ba3a2ea3a49df82bbc4b043ea8332f
shweta boraganve <Shweta.borganve@gmail.com>
2026-02-26 21:55:44 -0800

    Added modified conditional_statements.py

--- day-07/conditional_statements.py
+++ day-07/conditional_statements.py
@@ -1,9 +1,17 @@
+"""
+Traffic Light Control Program.
+
+This program takes traffic light color as input
+and displays the corresponding action.
+"""
+
 light = input("light color: ")
-if light == 'red':
+
+if light == "red":
     print("stop")
-elif light == 'yellow':
+elif light == "yellow":
     print("look")
-elif light == 'green':
+elif light == "green":
     print("go")
 else:
     print("not valid")
